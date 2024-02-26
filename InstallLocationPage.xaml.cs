bb2126e65342d81ce669c5e97d665bc35bdf9e9c
ValtsuL1 <linnavaltteri@gmail.com>
2024-02-26 15:16:33 +0200

    code cleanup

--- InstallLocationPage.xaml.cs
+++ InstallLocationPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Presentation;
 
 namespace InstallApp
@@ -30,7 +18,6 @@ namespace InstallApp
 
         private void locationBtn_Click(object sender, RoutedEventArgs e)
         {
-
             Microsoft.Win32.OpenFolderDialog dialog = new();
 
             dialog.Multiselect = false;
